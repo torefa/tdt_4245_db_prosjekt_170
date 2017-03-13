4f30b9c846fb5a3f743cad8978a3973242148889
Jarle Trollebø <aborysa@gmail.com>
2017-03-13 18:19:12 +0100

    Fixed another typo

--- src/program/Utholdenhet_ovelse.java
+++ src/program/Utholdenhet_ovelse.java
@@ -4,7 +4,7 @@ public class Utholdenhet_ovelse extends Ovelse{
 	long distanse_km;
 	long tid_min;
 	
-	public Utholdenhet_ovelse(long ovelse_id, String navn, String beskrivelse,long distanse_km,long tig_min){
+	public Utholdenhet_ovelse(long ovelse_id, String navn, String beskrivelse,long distanse_km,long tid_min){
 		super(ovelse_id,navn,beskrivelse);
 		this.distanse_km=distanse_km;
 		this.tid_min=tid_min;
