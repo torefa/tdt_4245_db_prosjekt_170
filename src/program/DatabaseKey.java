2e4611d8c8c014bb84d3b60c5f5571d37a5608dd
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2017-03-13 16:33:15 +0100

    database key without password

---
+++ src/program/DatabaseKey.java
@@ -0,0 +1,7 @@
+package program;
+
+public class DatabaseKey {
+	
+	protected static String KEY_URL = "jdbc:mysql://mysql.stud.ntnu.no/jarletr_db_prosjekt_170?user=jarletr_app_db_p&password=INSERTPASSWORD";
+
+}
