c881dc3c73afb894c98c218e84486d5e632664d8
hans.pieper <hans.pieper@rwth-aachen.de>
2017-03-13 17:43:20 +0100

    constructors added

--- src/program/Kondisjon_ovelse.java
+++ src/program/Kondisjon_ovelse.java
@@ -4,4 +4,11 @@ public class Kondisjon_ovelse extends Ovelse{
 	long belastning;
 	long repetisjoner;
 	long sett;
+	
+	public Kondisjon_ovelse(long ovelse_id, String navn, String beskrivelse,long belastning,long repetisjoner,long sett){
+		Super(ovelse_id,navn,beskrivelse);
+		this.belastning=belastning;
+		this.repetisjoner=repetisjoner;
+		this.sett=sett;
+	}
 }
--- src/program/Ovelse.java
+++ src/program/Ovelse.java
@@ -4,4 +4,10 @@ public class Ovelse{
 	long ovelse_id;
 	String navn;
 	String beskrivelse;
+	
+	public Ovelse(long ovelse_id, String navn, String beskrivelse){
+		this.ovelse_id=ovelse_id;
+		this.navn=navn;
+		this.beskrivelse=beskrivelse;
+	}
 }
