7c30ffdc4f402df4b92c1c636a202c6a092c52da
Jarle Trollebø <aborysa@gmail.com>
2017-03-14 19:49:42 +0100

    Removed DatabaseKey.java

--- src/program/DatabaseKey.java
+++
@@ -1,7 +0,0 @@
-package program;
-
-public class DatabaseKey {
-	
-	protected static String KEY_URL = "jdbc:mysql://mysql.stud.ntnu.no/jarletr_db_prosjekt_170?user=jarletr_app_db_p&password=A17O6b1r93hmS88";
-
-}
