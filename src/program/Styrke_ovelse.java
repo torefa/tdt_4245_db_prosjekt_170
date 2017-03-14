d802a0316131a3f7083cfbe81f02de3afbcb72ea
hans.pieper <hans.pieper@rwth-aachen.de>
2017-03-14 16:19:46 +0100

    added comments to ovelse classes

--- src/program/Styrke_ovelse.java
+++ src/program/Styrke_ovelse.java
@@ -1,9 +1,23 @@
 package program;
 
+/**
+ * Represents thev corresponding table in the database.
+ *
+ * @author Group 170
+ */
 public class Styrke_ovelse extends Ovelse{
 	long belastning;
 	long repetisjoner;
 	long sett;
+	
+	/**
+	* Creates a new strength exercise.
+	*
+	* @author Group 170
+	* @param belastning Weight that was used for this exercise.
+	* @param repetisjoner Number of repetitions of the exercise per {@link sett}.
+	* @param sett Number of sets of the exercise.
+	*/
 	
 	public Styrke_ovelse(long ovelse_id, String navn, String beskrivelse,long belastning,long repetisjoner,long sett){
 		super(ovelse_id,navn,beskrivelse);
