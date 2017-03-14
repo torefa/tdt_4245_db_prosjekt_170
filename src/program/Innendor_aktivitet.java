81f7096689feb09617170a10b1d8d86b1a90b97f
hans.pieper <hans.pieper@rwth-aachen.de>
2017-03-14 17:24:42 +0100

    added descriptions to the workout classes

--- src/program/Innendor_aktivitet.java
+++ src/program/Innendor_aktivitet.java
@@ -3,11 +3,22 @@ package program;
 import java.sql.Time;
 import java.util.Date;
 
+/**
+ * Represents the corresponding table in the database.
+ *
+ * @author Group 170
+ */
 public class Innendor_aktivitet extends Treningsokt{
 	long publikum;
 	int luft;
 	
-	
+	/**
+	* Creates a indoor activity.
+	*
+	* @author Group 170
+	* @param publikum Number of people watched you doing the workout.
+	* @param luft Climate conditions of the workout place.
+	*/
 	public Innendor_aktivitet(int trening_id,Date dato, Time tidspunkt, long varighet, int form,int prestasjon,String notat,long publikum,int luft){
 		super(trening_id,dato,tidspunkt,varighet,form,prestasjon,notat);
 		this.publikum = publikum;
--- src/program/Treningsokt.java
+++ src/program/Treningsokt.java
@@ -2,7 +2,14 @@ package program;
 
 import java.sql.Time;
 import java.util.Date;
-public class Treningsokt{
+
+/**
+ * Represents the corresponding table in the database.
+ * Is abstract since there should only be either indoor or outdoor activities.
+ *
+ * @author Group 170
+ */
+public abstract class Treningsokt{
 	long trening_id;
 	Date dato;
 	Time tidspunkt;
@@ -11,6 +18,18 @@ public class Treningsokt{
 	int prestasjon;
 	String notat;
 	
+	/**
+	* Creates an activity.
+	*
+	* @author Group 170
+	* @param trening_id Unic number in database.
+	* @param dato Starting date of workout.
+	* @param tidspunkt Startin time of workout.
+	* @param varighet Total time of the workout.
+	* @param form Rating of the shape the person was in.
+	* @param prestasjon Rating of how well the workout was done.
+	* @param notat Option to add arbitrary notes to the workout.
+	*/
 	public Treningsokt(int trening_id,Date dato, Time tidspunkt, long varighet, int form,int prestasjon,String notat) throws IndexOutOfBoundsException{
 		this.trening_id=trening_id;
 		this.dato=dato;
